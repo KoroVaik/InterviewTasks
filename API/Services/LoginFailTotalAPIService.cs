55d0c9d2334c0201fbc738fea9a117a90e201075
Rostyslav Porokhnia <Rostyslav.Porokhnia_EPAM@kantar.com>
2024-03-28 11:57:10 +0200

    fixes

--- API/Services/LoginFailTotalAPIService.cs
+++ API/Services/LoginFailTotalAPIService.cs
@@ -1,12 +1,6 @@
-﻿using A.Models;
-using API.Clients;
+﻿using API.Clients;
 using API.Helpers;
 using API.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace API.Services
 {
